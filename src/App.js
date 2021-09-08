33f260c6b2aeb8bd42bb6b6f67212b43f0dd7d7a
CongNV-Training <nguyen.van.cong@sun-asterisk.com>
2021-09-08 10:02:30 +0700

    ok chua

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const listStudent = ["Huyen", "Hoa", "Hung", "Long"];
+  const [selectNumber, setSelectNumber] = useState(1);
+  const [selectName, setSelectName] = useState(listStudent[0]);
+
+  const onClick1 = (event) => {
+    event.preventDefault();
+    if (selectNumber === 4) {
+      setSelectNumber(1);
+      setSelectName(listStudent[0]);
+    } else {
+      setSelectNumber(selectNumber + 1);
+      setSelectName(listStudent[selectNumber]);
+    }
+  };
+  const onClick2 = (event) => {
+    event.preventDefault();
+    if (selectNumber === 1) {
+      setSelectNumber(4);
+      setSelectName(listStudent[3]);
+    } else {
+      setSelectNumber(selectNumber - 1);
+      setSelectName(listStudent[selectNumber - 2]);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="listStudent">学生一覧：[Huyen, Hoa, Hung, Long]</div>
+      <div className="listStudent">位置：{selectNumber}</div>
+      <div className="listStudent">名前：{selectName}</div>
+      <button onClick={onClick2} className="listStudent">
+        prev
+      </button>
+      <button onClick={onClick1}>next</button>
     </div>
   );
 }
